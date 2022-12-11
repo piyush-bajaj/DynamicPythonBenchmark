c058998d8750daac1c62bff8ab87f74b4b65fe49
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2022-12-11 11:59:18 +0000

    python running test using bash

--- run_dypybench.py
+++ run_dypybench.py
@@ -1,9 +1,12 @@
 import argparse
+import subprocess
 import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--list", "-l", action="store_true", help="List all the projects DyPyBench supports")
+parser.add_argument(
+    "--test", "-t", type=int, nargs='+', help="Specify the project number to run test suite of the project")
 
 def printAllProjects():
     print("{:<8} {:<20} {:<50}".format("Number", "Project Name", "Repository URL"))
@@ -26,3 +29,30 @@ def setupProjects():
             temp.append(row[0])
             data.append(temp)
             original_data.append(row)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    setupProjects()
+
+    if args.list:
+        printAllProjects()
+    
+    if args.test:
+        projects = args.test
+        for project in projects:
+            if(project < 0 or project > 10):
+                print("Project number should be between 1 and 10")
+            else:
+                proj_name = str(data[project - 1][1])
+                proj_no = str(data[project - 1][0])
+                proj_flags = str(original_data[project - 1][1])
+                if(proj_flags == "rt"):
+                    proj_test_folder = str(original_data[project - 1][3])
+                elif(proj_flags == "t"):
+                    proj_test_folder = str(original_data[project - 1][2])
+                elif(proj_flags == "r"):
+                    proj_test_folder = ""
+                
+                output = subprocess.run(["./run-test.sh %s %s %s" %(proj_name, proj_no, proj_test_folder)
+                    ], shell=True, capture_output=True)
