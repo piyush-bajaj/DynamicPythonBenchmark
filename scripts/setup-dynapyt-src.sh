646f8159bf9d2c97d4e0b2818302d4709b39c81a
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-26 13:39:13 +0100

    install dynapyt source

---
+++ scripts/setup-dynapyt-src.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+ROOT_DIR=$(pwd)
+
+#update the dynapyt source from the forked repo.
+if [[ ! -d DynaPyt ]]
+then
+    git clone https://github.com/piyush-bajaj/DynaPyt.git $ROOT_DIR/DynaPyt
+else
+    git pull
+fi
+
+#checkout the lastest code and analysis from the custom branch
+git checkout piyush-bajaj-dypybench-analysis
